17c4711391d6d2d977b913fd058a00be23c2da75
Roberto Gomes <rjrg.snf20@uea.edu.br>
2021-06-04 18:43:30 -0400

    Update Apartments.cpp

--- Apartments.cpp
+++ Apartments.cpp
@@ -1,25 +1,32 @@
 #include<bits/stdc++.h>
 using namespace std;
-#define N ((int)2e5+123)
-
 
 int main(){
 	long long int n, m, k; 
-	cin>>n; cin>>m; cin>>k;
+	cin >> n >> m >> k;
 	
 	vector<long long int> a(n), b(m); 
 	for(int i = 0; i<n; ++i)
 		cin>>a[i];
 	for(int i = 0; i<m; ++i)
 		cin>>b[i];
-		
-	int ans;
 	
-	for(int i = 0; i<m; ++i){
-		for(int j = 0; j<n; ++j){
-			if((a[j]-k) > b[i] > (a[i]+k)){
-				++ans;
-			}
+	sort(a.begin(), a.end());
+	sort(b.begin(), b.end());
+	
+	int ans = 0, i, j;
+	
+	while(i < m && j < n){
+		if ((b[i]+k >= a[j])&&(b[i]-k <= a[j])){
+			ans++;
+			i++;
+			j++;
+		} else{
+			if(b[i] < a[j]){
+                i++;
+            } else if(b[i] > a[j]){
+                j++;
+            }
 		}
 	}
 	cout << ans << endl;	
